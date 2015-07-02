fec857b660030589ebae63f2cecc5084c75777ff
Amir <wowrainyman@gmail.com>
2015-07-02 07:56:09 +0330

    fix seo for expired products

--- catalog/model/common/seo.php
+++ catalog/model/common/seo.php
@@ -12,7 +12,7 @@ class ModelCommonSeo extends Model
     public function GetProductsByLink($buy_link)
     {
         $con_PU_db = $GLOBALS['con_PU_db'];
-        $sql_select_string = "SELECT * FROM `pu_subprofile_product` WHERE `buy_link` LIKE '%$buy_link%'";
+        $sql_select_string = "SELECT * FROM `pu_subprofile_product`  LEFT JOIN `pu_subprofile_verification` ON pu_subprofile_product.subprofile_id = `pu_subprofile_verification`.subprofile_id  LEFT JOIN `pu_subprofile` ON pu_subprofile.id = `pu_subprofile_product`.subprofile_id WHERE `buy_link` LIKE '%$buy_link%' AND `pu_subprofile_product`.status_id <> 0 AND (`pu_subprofile_product`.price <> 0 OR `pu_subprofile_product`.guarantee_price <> 0) AND ((`pu_subprofile_verification`.expire_date > CURDATE()) OR `pu_subprofile`.financial_exception = 1) AND `pu_subprofile_product`.`update_date` > DATE_SUB(CURDATE(), INTERVAL 60 DAY) AND(`pu_subprofile_product`.`availability` = 0 or`pu_subprofile_product`.`availability` = 2)";
         $result_select = mysqli_query($con_PU_db, $sql_select_string) or die(mysqli_error());
         $results = array();
         while ($row = mysqli_fetch_assoc($result_select)) {
